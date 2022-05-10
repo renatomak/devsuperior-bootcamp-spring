0b901d8cbb4bfa03057190a5b65f4340890bdb3c
renatomak <renato.mark.silva@gmail.com>
2022-05-10 11:06:50 -0300

    feat: validation response

---
+++ 03_validacao_e_seguranca/exemplo_aula_dscatalog/backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ValidationError.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ValidationError extends StandardError implements Serializable {
+    private static final long serialVersionUID = -8980680802489609207L;
+
+    private List<FieldMessage> erros = new ArrayList<>();
+
+    public void addError(String fieldName, String message) {
+        erros.add(new FieldMessage(fieldName, message));
+    }
+}
