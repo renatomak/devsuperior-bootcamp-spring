0ca3ce57257f1592831eecb1d2b7c117999ec182
renatomak <renato.mark.silva@gmail.com>
2022-05-15 07:14:09 -0300

    feat: implementing oAuth2 authorization server

--- 03_validacao_e_seguranca/exemplo_aula_dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ 03_validacao_e_seguranca/exemplo_aula_dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -1,11 +1,16 @@
 package com.devsuperior.dscatalog.entities;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
@@ -15,7 +20,7 @@ import java.util.Set;
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
     private static final long serialVersionUID = -102347617765968938L;
 
     @Id
@@ -40,4 +45,34 @@ public class User implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- 03_validacao_e_seguranca/exemplo_aula_dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
+++ 03_validacao_e_seguranca/exemplo_aula_dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -1,18 +1,22 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.dto.*;
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Role;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.RoleRepository;
 import com.devsuperior.dscatalog.repositories.UserRepository;
 import com.devsuperior.dscatalog.services.exceptions.DataBasesException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,9 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
-@Service
-public class UserService {
 
+@Service
+public class UserService implements UserDetailsService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
@@ -88,4 +94,14 @@ public class UserService {
         }
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            logger.error("User not found: " + username);
+            throw new UsernameNotFoundException("Email not found.");
+        }
+        logger.info("User found: " + username);
+        return user;
+    }
 }
