67b64955fbe57b94ffed720ac4ebf07113109de0
renatomak <renato.mark.silva@gmail.com>
2022-05-25 15:21:19 -0300

    feat: entity user

---
+++ 04_dominio_e_orm_atualizacoes/ds-learn/backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dslearnbds.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
